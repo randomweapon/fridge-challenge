523dc73c07e0a05315bb04a0e71f8006dd330d43
Ryan Spencer <ryan@justanotherapp.com>
2015-11-08 18:11:33 +1100

    validating CSV data and getting JSON data

--- src/index.php
+++ src/index.php
@@ -1,19 +1,52 @@
 <?php
+/**
+ *
+ */
 
-require 'vendor/autoload.php';
+require 'src/autoload.php';
 
 use App\Lib\Csv;
+use App\Lib\Json;
 use App\Lib\Args;
+use App\Lib\Messages;
 
-if ( isset($args) ) {
+date_default_timezone_set( 'Australia/Sydney' );
 
-    $arguments = new Args($args);
+$message = new Messages();
+
+if ( isset($argv) ) {
+
+    $arguments = new Args($argv);
+
+    if ($arguments->count() == 2) {
+
+        $ingredients = new Csv($arguments->get(0));
+
+        // check to see if there was any ingredients imported
+        if ( !$ingredients->count() ) {
+
+            // order takeout
+            $message->add("All ingredients are past their used by date, Order Takeout!");
+        }
+        else {
+            // sort ingredients by usedby date
+            $ingredients->sort();
+
+            $recipes = new Json($arguments->get(1));
+
+
+        }
+
+    }
+    else {
+        // invalid number of arguments passed
+        $message->add("Invalid number of arguments", true);
+    }
 
 }
 // no arguments passed, show help
 else {
-    //TODO : write commandline help
-    echo "help\n";
+    $message->addHelp();
 }
 
-//$csv = new Csv('');
+echo $message->__toString();
